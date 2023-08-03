e7e628d1447ebfd50cdd0a139579c7db099a0287
adomasr <adomas.razmus@gmail.com>
2023-08-03 02:11:15 +0100

    test deploy

--- api/getRandomImg.js
+++ api/getRandomImg.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 
 
 const getRandomImg = async (weather, timeOfDay)=> {
-    const RANDOM_IMG_API = process.env.RANDOM_IMG_API;      
-    const RANDOM_IMG_KEY = process.env.RANDOM_IMG_KEY;
+    // const RANDOM_IMG_API = process.env.RANDOM_IMG_API;      
+    // const RANDOM_IMG_KEY = process.env.RANDOM_IMG_KEY;
+    const RANDOM_IMG_API = 'https://api.unsplash.com/photos/random/';      
+    const RANDOM_IMG_KEY = 'XYMe11wvf2H6WeG3VzMj5QFbkZlplD0WCK2BCYPGIfI';
     const randomImgEndpoint = `?query=${weather},${timeOfDay},nature&orientation=portrait&client_id=${RANDOM_IMG_KEY}`;
 
     try {
